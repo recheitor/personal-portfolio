0038a76e5af2ed7b71da347b3bff59df609ccc52
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-10-11 16:57:34 +0200

    starting projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -9,9 +9,32 @@ const Projects = () => {
     return (
         <>
 
-            <Row className=''>
 
-            </Row>
+            <Row className='justify-content-center'>
+                <Col xs='11' lg='12' className='projects-card'>
+                    <Row className='justify-content-center mt-2'>
+                        <Col xs='11' lg='10' className=' text-center'>
+                            <h3>My <span>Projects</span></h3>
+                        </Col>
+                    </Row>
+                    <Row className='justify-content-center mt-2'>
+                        <Col xs='11' lg='10' className='projects-card-mini text-center'>
+                            JUMPER TRAVELER
+                        </Col>
+                    </Row>
+                    <Row className='justify-content-center'>
+                        <Col xs='11' lg='10' className='text-center'>
+                            UNO
+                        </Col>
+                    </Row>
+                    <Row className='justify-content-center'>
+                        <Col xs='11' lg='10' className='text-center'>
+                            UNO
+                        </Col>
+                    </Row>
+                </Col >
+
+            </Row >
 
 
         </>
