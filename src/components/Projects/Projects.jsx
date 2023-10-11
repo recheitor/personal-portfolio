5cfdeb47af98c099b6f6a3aff150e4dc843f9fbc
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-10-11 17:15:33 +0200

    projects update

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,35 +8,37 @@ const Projects = () => {
 
     return (
         <>
-
-
             <Row className='justify-content-center'>
-                <Col xs='11' lg='12' className='projects-card'>
+                <Col xs='12' lg='12' className='projects-card'>
                     <Row className='justify-content-center mt-2'>
-                        <Col xs='11' lg='10' className=' text-center'>
+                        <Col xs='12' lg='10' className=' text-center'>
                             <h3>My <span>Projects</span></h3>
                         </Col>
                     </Row>
-                    <Row className='justify-content-center mt-2'>
-                        <Col xs='11' lg='10' className='projects-card-mini text-center'>
-                            JUMPER TRAVELER
+                    <Row className='justify-content-evenly mt-2 mb-4'>
+                        <Col xs='10' lg='3' className='projects-card-mini text-center mt-2'>
+                            <h4>COMPI ROOMS</h4>
+                            <a href="https://compirooms.netlify.app/" target="_blank"><img src="https://res.cloudinary.com/dbtmrinwa/image/upload/v1697036164/czuemis1mfinxmuhgpbs.jpg" alt="Project screenshot" /></a>
+                            <h4>Developed with:</h4>
+                            <h5>ExpressJS - MongoDB</h5>
+                            <h5>ReactJS - NodeJS</h5>
                         </Col>
-                    </Row>
-                    <Row className='justify-content-center'>
-                        <Col xs='11' lg='10' className='text-center'>
-                            UNO
+
+                        <Col xs='10' lg='3' className='projects-card-mini text-center mt-2'>
+                            <h4>ONLY PIZZAS</h4>
+                            <a href="https://onlypizzas.fly.dev/" target="_blank"><img src="https://res.cloudinary.com/dbtmrinwa/image/upload/v1697036131/uiriutfupwqjhlgwejf7.jpg" alt="Project screenshot" /></a>
+                            <h4>Developed with:</h4>
+                            <h5>ExpressJS - MongoDB - NodeJS</h5>
                         </Col>
-                    </Row>
-                    <Row className='justify-content-center'>
-                        <Col xs='11' lg='10' className='text-center'>
-                            UNO
+                        <Col xs='10' lg='3' className='projects-card-mini text-center mt-2'>
+                            <h4>JUMPER TRAVELER</h4>
+                            <a href="https://recheitor.github.io/M1Project/" target="_blank">  <img src="https://res.cloudinary.com/dbtmrinwa/image/upload/v1697036184/otd2rhxmrgor55qrmtyg.jpg" alt="Project screenshot" /></a>
+                            <h4>Developed with:</h4>
+                            <h5>HTML - CSS - NodeJS</h5>
                         </Col>
                     </Row>
                 </Col >
-
             </Row >
-
-
         </>
     )
 }
