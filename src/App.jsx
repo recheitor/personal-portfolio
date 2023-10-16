7ca8067459b9d49137a5a8ef4e20db296964dc68
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-10-16 23:23:16 +0200

    whyyyy

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,21 @@ import './App.css'
 import { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
 
+
+window.addEventListener('load', () => {
+  const linkElement = document.createElement('link');
+  linkElement.href = 'https://fonts.googleapis.com/css2?family=Lexend:wght@300;500;800&display=swap';
+  linkElement.rel = 'stylesheet';
+
+  document.head.appendChild(linkElement);
+
+});
+
 function App() {
   const [language, setLanguage] = useState('ENG')
 
 
 
-  window.addEventListener('load', () => {
-    const linkElement = document.createElement('link');
-    linkElement.href = 'https://fonts.googleapis.com/css2?family=Lexend:wght@300;500;800&display=swap';
-    linkElement.rel = 'stylesheet';
-
-    document.head.appendChild(linkElement);
-
-  });
 
 
 
