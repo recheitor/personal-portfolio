d12d8a496bf0b9494af019f1ef53f182f7b3465a
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-10-16 19:34:36 +0200

    robots

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage/HomePage'
 
-
 const AppRoutes = ({ language }) => {
 
     return (
         <Routes>
             <Route path={'/'} element={<HomePage language={language} />} />
+            <Route path="/robots.txt" render={() => (
+                <pre>
+                    {` User-agent: *
+                        Allow: / `}
+                </pre>
+            )} />
         </Routes>
     )
 }
