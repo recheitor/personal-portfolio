f8d86351558dc4d02647f6acbf6ac6e82461d1d5
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-10-16 15:40:19 +0200

    image

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -19,7 +19,7 @@ const Projects = ({ language }) => {
                         <Col xs='10' lg='3' className='projects-card-mini text-center mt-3' role="listitem">
                             <h4>COMPI ROOMS</h4>
                             <a href="https://compirooms.netlify.app/" target="_blank">
-                                <img src="https://res.cloudinary.com/dbtmrinwa/image/upload/f_auto,q_auto/czuemis1mfinxmuhgpbs" alt="COMPI ROOMS Project screenshot" role="img" aria-label="COMPI ROOMS Project Screenshot" />
+                                <img src="https://res.cloudinary.com/dbtmrinwa/image/upload/t_mini3/czuemis1mfinxmuhgpbs.jpg" alt="COMPI ROOMS Project screenshot" role="img" aria-label="COMPI ROOMS Project Screenshot" />
                             </a>
                             <h4>{language === 'ENG' ? 'Developed with:' : 'Desarrollado con:'}</h4>
                             <h5>ExpressJS - MongoDB</h5>
@@ -29,7 +29,7 @@ const Projects = ({ language }) => {
                         <Col xs='10' lg='3' className='projects-card-mini text-center mt-3' role="listitem">
                             <h4>ONLY PIZZAS</h4>
                             <a href="https://onlypizzas.fly.dev/" target="_blank">
-                                <img src="https://res.cloudinary.com/dbtmrinwa/image/upload/f_auto,q_auto/uiriutfupwqjhlgwejf7" alt="ONLY PIZZAS Project screenshot" role="img" aria-label="ONLY PIZZAS Project Screenshot" />
+                                <img src="https://res.cloudinary.com/dbtmrinwa/image/upload/t_mini5/uiriutfupwqjhlgwejf7.jpg" alt="ONLY PIZZAS Project screenshot" role="img" aria-label="ONLY PIZZAS Project Screenshot" />
                             </a>
                             <h4>{language === 'ENG' ? 'Developed with:' : 'Desarrollado con:'}</h4>
                             <h5>ExpressJS - MongoDB - NodeJS</h5>
@@ -37,7 +37,7 @@ const Projects = ({ language }) => {
                         <Col xs='10' lg='3' className='projects-card-mini text-center mt-3' role="listitem">
                             <h4>JUMPER TRAVELER</h4>
                             <a href="https://recheitor.github.io/M1Project/" target="_blank">
-                                <img src="https://res.cloudinary.com/dbtmrinwa/image/upload/f_auto,q_auto/otd2rhxmrgor55qrmtyg" alt="JUMPER TRAVELER Project screenshot" role="img" aria-label="JUMPER TRAVELER Project Screenshot" />
+                                <img src="https://res.cloudinary.com/dbtmrinwa/image/upload/t_mini6/otd2rhxmrgor55qrmtyg.jpg" alt="JUMPER TRAVELER Project screenshot" role="img" aria-label="JUMPER TRAVELER Project Screenshot" />
                             </a>
                             <h4>{language === 'ENG' ? 'Developed with:' : 'Desarrollado con:'}</h4>
                             <h5>HTML - CSS - NodeJS</h5>
