8c6a341cdc062da9eaf17b6c199291a558430402
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-10-16 23:45:56 +0200

    same render shit

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,23 @@ import Navigation from './components/Navigation/Navigation'
 import AppRoutes from './routes/AppRoutes'
 import Footer from './components/Footer/Footer'
 import './App.css'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Container } from 'react-bootstrap'
 
 
-window.addEventListener('load', () => {
-  const linkElement = document.createElement('link');
-  linkElement.href = 'https://fonts.googleapis.com/css2?family=Lexend:wght@300;500;800&display=swap';
-  linkElement.rel = 'stylesheet';
 
-  document.head.appendChild(linkElement);
-
-});
 
 function App() {
+
   const [language, setLanguage] = useState('ENG')
-
-
-
-
-
-
 
   return (
     <>
-
-
       <Navigation language={language} setLanguage={setLanguage} />
       <Container>
         <AppRoutes language={language} />
       </Container>
       <Footer />
-
     </>
   )
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,40 +1,13 @@
-import { useEffect, useState } from 'react'
+import Hero from '../../components/Hero/Hero'
 import Background from '../../components/Background/Background'
 import Contact from '../../components/Contact/Contact'
-import Hero from '../../components/Hero/Hero'
 import Projects from '../../components/Projects/Projects'
 import Qualities from '../../components/Qualities/Qualities'
 import './HomePage.css'
 import { Col, Row } from 'react-bootstrap'
+
+
 const HomePage = ({ language }) => {
-
-    const [componentsLoaded, setComponentsLoaded] = useState({
-        qualities: false,
-        background: false,
-        projects: false,
-        contact: false,
-    });
-
-
-
-    // const loadComponents = async () => {
-    //     await new Promise((resolve) => {
-    //         window.addEventListener('load', () => {
-    //             resolve();
-    //             setComponentsLoaded({ qualities: true, background: true, projects: true, contact: true });
-    //         });
-    //     });
-    // };
-
-    // loadComponents();
-
-
-
-
-
-
-
-
     return (
         <>
             <Row className='justify-content-center' role="region" aria-label={language === 'ENG' ? 'Homepage' : 'Página de inicio'}>
@@ -53,26 +26,25 @@ const HomePage = ({ language }) => {
             <Row className='justify-content-center'>
                 <Col xs='12' lg='10'>
                     <section>
-                        {componentsLoaded.qualities && (
-                            <>
-                                <Qualities language={language} />
-                                <Background language={language} />
-                            </>
-                        )}
+                        <>
+                            <Qualities language={language} />
+                            <Background language={language} />
+                        </>
+
                     </section>
                 </Col>
             </Row>
             <Row>
                 <Col xs='12' lg='12'>
                     <section id="projects-section" role="region" aria-label={language === 'ENG' ? 'Projects Section' : 'Sección de Proyectos'}>
-                        {componentsLoaded.projects && <Projects language={language} />}
+                        <Projects language={language} />
                     </section>
                 </Col>
             </Row>
             <Row>
                 <Col xs='12' lg='12'>
                     <section id="contact-me-section" role="region" aria-label={language === 'ENG' ? 'Contact Me Section' : 'Sección de Contacto'}>
-                        {componentsLoaded.contact && <Contact language={language} />}
+                        && <Contact language={language} />
                     </section>
                 </Col>
             </Row>
