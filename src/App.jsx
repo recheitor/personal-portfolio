1c6eb4f0d334443ca8a01617899ec4ebe40ddcc5
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-10-16 23:07:35 +0200

    still testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,41 @@ import Navigation from './components/Navigation/Navigation'
 import AppRoutes from './routes/AppRoutes'
 import Footer from './components/Footer/Footer'
 import './App.css'
-import { useEffect, useState } from 'react'
+import { useEffect, useLayoutEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
 
 function App() {
   const [language, setLanguage] = useState('ENG')
 
-  const [font, setFont] = useState(false)
+  const [loaded, setLoaded] = useState(false);
 
-  const loadComponents = async () => {
-    await new Promise((resolve) => {
-      window.addEventListener('load', () => {
-        resolve();
-        const linkElement = document.createElement('link');
-        linkElement.href = 'https://fonts.googleapis.com/css2?family=Lexend:wght@300;500;800&display=swap';
-        linkElement.rel = 'stylesheet';
+  useLayoutEffect(() => {
+    if (document.readyState === 'complete') {
 
-        document.head.appendChild(linkElement);
-        setFont(true)
-      });
-    });
-  };
-  loadComponents();
+      setLoaded(true);
+    } else {
+      const handleLoad = () => {
+        setLoaded(true);
+      };
+      window.addEventListener('load', handleLoad);
 
+      const linkElement = document.createElement('link');
+      linkElement.href = 'https://fonts.googleapis.com/css2?family=Lexend:wght@300;500;800&display=swap';
+      linkElement.rel = 'stylesheet';
 
+      document.head.appendChild(linkElement);
+      return () => {
+        window.removeEventListener('load', handleLoad);
+      };
+    }
+  }, []);
 
 
 
 
   return (
     <>
-      {font && (
+      {loaded && (
         <>
           <Navigation language={language} setLanguage={setLanguage} />
           <Container>
