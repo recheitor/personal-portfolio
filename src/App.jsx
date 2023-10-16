ddd5f7ecd786bfe7faa96e585368af7c6a1a6247
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-10-16 21:13:05 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
       {/* <link rel="preconnect" href="https://fonts.googleapis.com" async />
       <link rel="preconnect" href="https://fonts.gstatic.com" async /> */}
-      <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;500;800&display=swap" rel="stylesheet" async />
+      {/* <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;500;800&display=swap" rel="stylesheet" async /> */}
       <Navigation language={language} setLanguage={setLanguage} />
       <Container>
         <AppRoutes language={language} />
