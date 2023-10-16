0f4657d1e8d9665047c1bbd9f1179f0a22d560d2
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-10-16 20:10:30 +0200

    lazy test

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
-import Background from '../../components/Background/Background'
-import Contact from '../../components/Contact/Contact'
-import Hero from '../../components/Hero/Hero'
-import Projects from '../../components/Projects/Projects'
-import Qualities from '../../components/Qualities/Qualities'
-import './HomePage.css'
-import { Col, Row } from 'react-bootstrap'
-const HomePage = ({ language }) => {
+import { lazy, Suspense } from 'react';
+import './HomePage.css';
+import { Col, Row } from 'react-bootstrap';
 
+const Hero = lazy(() => import('../../components/Hero/Hero'));
+const Qualities = lazy(() => import('../../components/Qualities/Qualities'));
+const Background = lazy(() => import('../../components/Background/Background'));
+const Projects = lazy(() => import('../../components/Projects/Projects'));
+const Contact = lazy(() => import('../../components/Contact/Contact'));
+
+const HomePage = ({ language }) => {
     return (
         <>
             <Row className='justify-content-center' role="region" aria-label={language === 'ENG' ? 'Homepage' : 'Página de inicio'}>
@@ -18,36 +20,42 @@ const HomePage = ({ language }) => {
             <Row>
                 <Col xs='12' lg='12'>
                     <section role="main">
-                        <Hero language={language} />
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Hero language={language} />
+                        </Suspense>
                     </section>
                 </Col>
             </Row>
             <Row className='justify-content-center'>
                 <Col xs='12' lg='10'>
                     <section>
-                        <Qualities language={language} />
-                        <Background language={language} />
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Qualities language={language} />
+                            <Background language={language} />
+                        </Suspense>
                     </section>
                 </Col>
             </Row>
             <Row>
                 <Col xs='12' lg='12'>
                     <section id="projects-section" role="region" aria-label={language === 'ENG' ? 'Projects Section' : 'Sección de Proyectos'}>
-                        <Projects language={language} />
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Projects language={language} />
+                        </Suspense>
                     </section>
                 </Col>
             </Row>
             <Row>
                 <Col xs='12' lg='12'>
                     <section id="contact-me-section" role="region" aria-label={language === 'ENG' ? 'Contact Me Section' : 'Sección de Contacto'}>
-                        <Contact language={language} />
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Contact language={language} />
+                        </Suspense>
                     </section>
                 </Col>
             </Row>
-
-
         </>
-    )
-}
+    );
+};
 
-export default HomePage
+export default HomePage;
