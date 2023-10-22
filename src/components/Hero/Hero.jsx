7811e945cb1a88a2cf8054cfc3223ee21eea69a6
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-10-22 19:33:04 +0200

    load

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,26 +1,11 @@
 import { Col, Row, } from 'react-bootstrap'
 import './Hero.css'
-import { useEffect, useState } from 'react';
 const Hero = ({ language }) => {
-
-    const [loaded, setLoaded] = useState(false);
-
-    useEffect(() => {
-        const handleLoad = () => {
-            setLoaded(true);
-        };
-
-        window.addEventListener('load', handleLoad);
-
-        return () => {
-            window.removeEventListener('load', handleLoad);
-        };
-    }, []);
 
     return (
         <>
             <div className='hero-background'>
-                <Row className={`hero justify-content-center align-items-center ${loaded ? 'fade-in' : ''}`} role="banner" aria-label={language === 'ENG' ? 'Hero Section' : 'Sección del héroe'}>
+                <Row className='hero justify-content-center align-items-center' role="banner" aria-label={language === 'ENG' ? 'Hero Section' : 'Sección del héroe'}>
                     <Col xs='8' lg='7' className='d-flex flex-column align-items-center text-center'>
                         <h1 role="heading" aria-level="1">
                             {language === 'ENG' ? 'Hey! I am' : '¡Hola! Soy'}
