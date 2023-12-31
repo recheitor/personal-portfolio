6015d527868532a69e4a145bbbea03c44bb18bcc
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-12-31 23:16:17 +0100

    lazy

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,14 @@
 import Hero from '../../components/Hero/Hero'
-import Background from '../../components/Background/Background'
-import Contact from '../../components/Contact/Contact'
-import Projects from '../../components/Projects/Projects'
+// import Background from '../../components/Background/Background'
+// import Contact from '../../components/Contact/Contact'
+// import Projects from '../../components/Projects/Projects'
 import Qualities from '../../components/Qualities/Qualities'
 import './HomePage.css'
 import { Col, Row } from 'react-bootstrap'
-
+import React, { Suspense } from 'react';
+const Background = React.lazy(() => import('../../components/Background/Background'))
+const Projects = React.lazy(() => import('../../components/Projects/Projects'))
+const Contact = React.lazy(() => import('../../components/Contact/Contact'))
 
 const HomePage = ({ language }) => {
     return (
@@ -22,21 +25,30 @@ const HomePage = ({ language }) => {
                 <Col xs='12' lg='10' >
                     <section id="about-me-section">
                         <Qualities language={language} />
-                        <Background language={language} />
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Background />
+                        </Suspense>
+                        {/* <Background language={language} /> */}
                     </section>
                 </Col>
             </Row>
             <Row>
                 <Col xs='12' lg='12'>
                     <section id="projects-section" role="region" aria-label={language === 'ENG' ? 'Projects Section' : 'Sección de Proyectos'}>
-                        <Projects language={language} />
+                        {/* <Projects language={language} /> */}
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Projects />
+                        </Suspense>
                     </section>
                 </Col>
             </Row>
             <Row>
                 <Col xs='12' lg='12'>
                     <section id="contact-me-section" role="region" aria-label={language === 'ENG' ? 'Contact Me Section' : 'Sección de Contacto'}>
-                        <Contact language={language} />
+                        {/* <Contact language={language} /> */}
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Contact />
+                        </Suspense>
                     </section>
                 </Col>
             </Row>
